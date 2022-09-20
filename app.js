d7bed055bf86928dab2f6527a8adb7e2a47b216c
elliotabrahamsson <elliotsson@outlook.com>
2022-09-20 11:54:14 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+Add = () => {
+    Name = document.getElementById('name').value;
+    Phone = document.getElementById('telefonnummer').value;
+    Adress = document.getElementById('Adress').value;
+
+}
