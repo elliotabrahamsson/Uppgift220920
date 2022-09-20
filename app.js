81c304df7db952559c63561399131bd3a102d23d
elliotabrahamsson <elliotsson@outlook.com>
2022-09-20 12:36:37 +0200

    writes out list from the three text boxes

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ var Customers = [];
 var Person = {
     Name:'',
     Phone:'',
-    Adress:'',
+    Adress:''
 }
 
 Add = () => {
-    Name = document.getElementById('name').value;
+    Name = document.getElementById('namn').value;
     Phone = document.getElementById('telefonnummer').value;
-    Adress = document.getElementById('Adress').value;
+    Adress = document.getElementById('Leveransadress').value;
+    Person = {Name: Name, Phone: Phone, Adress: Adress};
+    Customers.push(Person);
 
+    CustomerList = document.getElementById('customers');
+
+    CustomerList.innerHTML ='';
+
+    Customers.forEach (person => {
+        NameDetail = document.createElement('li');
+        NameDetail.innerHTML = person.Name;
+        CustomerList.appendChild(NameDetail);
+
+        PhoneDetail = document.createElement('li');
+        PhoneDetail.innerHTML = person.Phone;
+        CustomerList.appendChild(PhoneDetail);
+
+        AdressDetail = document.createElement('li');
+        AdressDetail.innerHTML = person.Adress;
+        CustomerList.appendChild(AdressDetail);
+    });
 }
